f1ea6815e4555de48401150d2ee971ca8cdc0370
chrissyrw <nshuti920@gmail.com>
2026-09-17 08:34:33 +0200

    feat: connect World Director to KLG AI Director

--- klg/src/ai/AIDirector.js
+++ klg/src/ai/AIDirector.js
@@ -1,12 +1,16 @@
+import { WorldDirector } from './WorldDirector.js';
+
 export class AIDirector {
   constructor(game) {
     this.game = game;
     this.elapsed = 0;
     this.phase = 'calm';
+    this.worldDirector = new WorldDirector(game);
   }
 
   update(dt) {
     this.elapsed += dt;
+    this.worldDirector.update(dt);
     if (this.elapsed < 12) return;
     this.elapsed = 0;
     const hour = this.game.state.get().world.time / 60;
